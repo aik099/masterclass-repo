5338c426ea8efe8a684a3ff0a2cdfbedb2e5bc3f
Alexander Obuhovich <aik.bold@gmail.com>
2013-09-09 23:17:39 +0300

    Namespacing and adding auto-loader.

--- public/index.php
+++ public/index.php
@@ -1,14 +1,31 @@
 <?php
 
+define('FULL_PATH', dirname(__DIR__));
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 session_start();
 
+function upvote_autoload($className)
+{
+	$className = ltrim($className, '\\');
+	$fileName = '';
+
+	if ( $lastNsPos = strrpos($className, '\\') ) {
+		$namespace = substr($className, 0, $lastNsPos);
+		$className = substr($className, $lastNsPos + 1);
+		$fileName = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+	}
+	$fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+
+	require $fileName;
+}
+
+set_include_path(get_include_path() . ':' . FULL_PATH);
+spl_autoload_register('upvote_autoload');
+
 $config = require_once('../config.php');
-require_once '../MasterController.php';
 
-require_once '../Comment.php';
-require_once '../User.php';
-require_once '../Story.php';
-require_once '../index.php';
-
-$framework = new MasterController($config);
+$framework = new \Upvote\Library\Front\Controller($config);
 echo $framework->execute();
